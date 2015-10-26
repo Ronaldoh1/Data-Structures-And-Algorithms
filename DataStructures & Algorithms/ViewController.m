07db5077ad355e82684bd6a7b0b30cbe40df9ce7
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-10-26 00:01:04 -0700

    able to find the max value of an array

--- DataStructures & Algorithms/ViewController.m
+++ DataStructures & Algorithms/ViewController.m
@@ -17,11 +17,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    //1. Find the index of Max Value
+    NSArray *testArray = [NSArray arrayWithObjects:@4, @3, @8, @10, @1, nil];
+
+   NSUInteger indexOfMaximumElement =  [self findIndexOfMaximumElement:testArray];
+    NSLog(@"The indexOfMaximum Element is %lu", (unsigned long)indexOfMaximumElement);
+
+
+
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Arrays 
+
+//Find max element in an array.
+
+//**Stragegy - the stragety is to assign the maxValue to the first object of the arrray and set the indexOfMaxValue to zero and update these as you iterate through the array and compare if the ith element of the array is greater than the maximum value ..if it is then that is your new maxValue. 
+-(NSUInteger )findIndexOfMaximumElement:(NSArray *)array{
+    id maximumValue = [array firstObject];
+
+    NSUInteger indexOfMaximumValue = 0;
+
+    for (int i = 0; i<array.count; i++) {
+
+        if ([array[i] compare:maximumValue] == NSOrderedDescending) {
+
+            maximumValue = array[i];
+            indexOfMaximumValue = i;
+        }
+    }
+
+    return indexOfMaximumValue;
+
+
 }
 
 @end
