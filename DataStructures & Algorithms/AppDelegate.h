d3f565772136814067f034003b0a8e0d2c821361
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-10-25 23:30:41 -0700

    Initial Commit

---
+++ DataStructures & Algorithms/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DataStructures & Algorithms
+//
+//  Created by Ronald Hernandez on 10/25/15.
+//  Copyright © 2015 Wahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
